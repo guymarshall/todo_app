d4453d301fe9c12dae973d23650606a22a9f491e
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-05 12:29:08 +0000

    Put all starting prints into one println!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 mod user_input;
 
 fn main() {
-    println!("Todo App! What do you want to do?");
-    println!("1     List all todos");
-    println!("2     Add todo");
-    println!("3     Delete todo");
+    println!("Todo App! What do you want to do?\n1     List all todos\n2     Add todo\n3     Delete todo");
     let choice: i32 = user_input::get_user_input("Enter your choice:");
 
     match choice {
